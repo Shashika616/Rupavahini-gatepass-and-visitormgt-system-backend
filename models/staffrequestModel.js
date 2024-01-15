7cc6cb78d3fbda8b4d5f5e669febd8f26d85fe89
dinukrana2000 <dinuk2000rana@gmail.com>
2024-01-16 00:17:57 +0530

    create staffrequestModel.js

---
+++ models/staffrequestModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const staffrequestSchema = new Schema({
+    empId:{
+        type: String,
+        required: true,
+        
+    },
+    reason:{
+        type: String,
+        required: true,
+    },
+    currentDate:{
+        type: Date,
+        required: true,
+        default: Date.now(),
+    },
+    givenTimeout:{
+        type: String,
+        required: true,
+    },
+    givenTimein:{
+        type: String,
+        required: true,
+    },
+    details:{
+        type: String,
+    },
+   
+});
+
+module.exports = mongoose.model('Staffrequest', staffrequestSchema);
