b285d3dac7d22a13f6cf1f4b0bdf0934ef14a4de
dinukrana2000 <dinuk2000rana@gmail.com>
2024-01-16 00:22:06 +0530

    create staffcomplaintModel.js

---
+++ models/staffcomplaintModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const staffcomplaintSchema = new Schema({
+    empId:{
+        type: String,
+        required: true,
+        
+    },
+    title:{
+        type: String,
+        required: true,
+    },
+    complaintDate:{
+        type: Date,
+        required: true,
+    },
+    details:{
+        type: String,
+        required: true,
+    },
+});
