4731ba272a61ef5aa71c5d00fe4a7ed9920d2d3d
dinukrana2000 <dinuk2000rana@gmail.com>
2024-01-16 00:39:39 +0530

    create staffgateModel.js

---
+++ models/staffgateModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const staffgateSchema = new Schema({
+    currentDate: {
+        type: Date,
+        required: true,
+        default: Date.now(),
+    },
+    inOut: {    
+        type: String,
+        required: true,
+    },
+    time: {
+        type: Date,
+        required: true,
+    },
+    VehicleNo: {
+        type: String,
+    },
+    empId: {
+        type: String,
+        required: true,
+    },
+    position: {
+        type: String,
+        required: true,
+    },
+});
+
+module.exports = mongoose.model("Staffgate", staffgateSchema);
