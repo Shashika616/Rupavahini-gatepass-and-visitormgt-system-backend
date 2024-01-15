a639ff521a71280a33edb60300ec99adaccd1be8
dinukrana2000 <dinuk2000rana@gmail.com>
2024-01-15 23:57:20 +0530

    create appoinmentModel.js

---
+++ models/appoinmentModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const appoinmentSchema = new Schema({
+    requesterName:{
+        type: String,
+        required: true,
+    },
+    requesteremail:{
+        type: String,
+        required: true,
+    },
+    requesterNIC:{
+        type: String,
+        required: true,
+    },
+    requesterPhoneno:{
+        type: String,
+        required: true,
+    },
+    officerName:{
+        type: String,
+        required: true,
+    },
+
+    appoinmentDate:{
+        type: Date,
+        required: true,
+    },
+    appoinmentTime:{
+        type: String,
+        required: true,
+    },
+    appoinmentReason:{
+        type: String,
+        required: true,
+    },
+    
+});
+    
+module.exports = mongoose.model('Appoinment', appoinmentSchema);
