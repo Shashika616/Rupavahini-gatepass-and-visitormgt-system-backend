dd24e5b1828a48a2d06a5678c49e3d018696f3e7
Shashika616 <shashikauda@gmail.com>
2024-01-17 01:58:57 +0530

    Route for user fetch controller

---
+++ routes/routeUserFetch.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const {userstaffRegister} = require("../controllers/fetchuserdatacontroller");
+const UserData = require("../controllers/fetchuserdatacontroller");
+
+router.post("userData",UserData);
+
+module.exports = router;
