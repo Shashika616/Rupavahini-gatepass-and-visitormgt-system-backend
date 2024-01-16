3a90980b302565b6a58c6b6846aee800849aea03
Shashika616 <shashikauda@gmail.com>
2024-01-17 01:51:17 +0530

    controller file to extract staff data from a token

---
+++ controllers/fetchstaffdatacontroller.js
@@ -0,0 +1,69 @@
+const Userstaff = require("../models/userstaffModel");
+const bcrypt = require('bcryptjs');  // Hash Library for password
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+const emailValidator = require("email-validator");
+
+const JWT_SECRET = process.env.JWT_SECRET;
+const NODEMAILER_USER = process.env.NODEMAILER_USER;
+const NODEMAILER_PASSWORD = process.env.NODEMAILER_PASSWORD;
+const RECEIVER_EMAIL = process.env.RECEIVER_EMAIL;
+
+const StaffData = async (req, res) => {
+    const { token } = req.body;
+  
+    try {
+      const user = jwt.verify(token, JWT_SECRET);
+  
+      // If the token is valid, proceed to fetch user data
+      const empID = user.empID; // Assuming the username is stored in the JWT payload
+  
+      try {
+        const staffData = await Userstaff.findOne({ empID });
+  
+        if (staffData) {
+          res.json({ status: "ok", data: staffData });
+        } else {
+          res.status(404).json({ status: "error", data: "User not found" });
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+        res.status(500).json({ status: "error", data: "Internal Server Error" });
+      }
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        res.status(401).json({ status: "error", data: "Token expired" });
+      } else {
+        console.error("Token verification error:", error);
+        res.status(500).json({ status: "error", data: "Internal Server Error" });
+      }
+    }
+  };
+
+  /*
+  const StaffData = async(req,res)=>{
+    const{token}=req.body;
+    try {
+        const user = jwt.verify(token,JWT_SECRET,(err,res)=>{
+            if(err){
+                return "token expired";
+            }
+            return res;  //return user as decoded payload(include email)  (Return value is always returned to the first callback function)
+        });
+        console.log(user);
+        if(user=="token expired"){
+            return res.send({status:"error",data:"token expired"});
+        }
+        const staffID = user.empID;
+        User.findOne({empID:staffID}).then((data)=>{
+            res.send({status:"ok",data:data});
+        }).catch((error)=>{
+            res.send({status:"error",data:error});
+        });
+    } catch (error) {
+        
+    }
+};
+*/
+  
+  module.exports = StaffData;
