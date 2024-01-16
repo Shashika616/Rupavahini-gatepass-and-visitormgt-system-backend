76d32bbaf2b60d28faf6db97060b64ad0b72a36c
Shashika616 <shashikauda@gmail.com>
2024-01-17 01:59:22 +0530

    Route for user login controller

---
+++ routes/routeUserLogin.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const {userstaffRegister} = require("../controllers/userlogincontroller");
+const LoginUser = require("../controllers/userlogincontroller");
+
+router.post("LoginUser",LoginUser);
+
+module.exports = router;
