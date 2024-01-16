dbd71795d26820f270c9b617811cdbc14163018b
Shashika616 <shashikauda@gmail.com>
2024-01-17 01:56:25 +0530

    The controller file when a normal user forgets a pasword and recover through email (Not Done)

---
+++ controllers/userforgotpasswordcontroller.js
@@ -0,0 +1,12 @@
+const User = require("../models/userModel");
+const bcrypt = require('bcryptjs');  // Hash Library for password
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+const emailValidator = require("email-validator");
+
+const JWT_SECRET = process.env.JWT_SECRET;
+const NODEMAILER_USER = process.env.NODEMAILER_USER;
+const NODEMAILER_PASSWORD = process.env.NODEMAILER_PASSWORD;
+const RECEIVER_EMAIL = process.env.RECEIVER_EMAIL;
+
+// Code to be implemented
