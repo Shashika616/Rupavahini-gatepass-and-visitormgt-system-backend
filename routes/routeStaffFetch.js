6b2227d1ce2f5a313ca27688b15649eec51f5514
Shashika616 <shashikauda@gmail.com>
2024-01-17 01:57:37 +0530

    Route for staff fetch controller

---
+++ routes/routeStaffFetch.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const {userstaffRegister} = require("../controllers/fetchstaffdatacontroller");
+const StaffData = require("../controllers/fetchstaffdatacontroller");
+
+router.post("StaffData",StaffData);
+
+module.exports = router;
