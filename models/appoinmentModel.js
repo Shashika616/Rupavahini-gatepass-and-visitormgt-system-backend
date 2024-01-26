bccb51568ba7c3b9e95f6265ddb0bc3e1f666ebf
Shashika616 <shashikauda@gmail.com>
2024-01-27 00:48:19 +0530

    new appointment model with a username field

--- models/appoinmentModel.js
+++ models/appoinmentModel.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const appoinmentSchema = new Schema({
+    username:{
+        type: String,
+        required: true,
+    },
     requesterName:{
         type: String,
         required: true,
