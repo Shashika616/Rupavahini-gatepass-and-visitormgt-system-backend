137f41b8c3f5552e8ea68724722acab5a2810e06
Shashika616 <shashikauda@gmail.com>
2024-01-29 18:43:08 +0530

    route file for user controller added

---
+++ routes/routeuser.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const {userRegister} = require("../controllers/usercontroller");
+
+router.post("/userRegister",userRegister);
+
+module.exports = router;
