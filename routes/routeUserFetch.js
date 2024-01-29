dc892b92fabac13c6a385344617c3773b72aa85b
Shashika616 <shashikauda@gmail.com>
2024-01-29 18:43:42 +0530

    route file for user fetch data modified

--- routes/routeUserFetch.js
+++ routes/routeUserFetch.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const {userstaffRegister} = require("../controllers/fetchuserdatacontroller");
 const UserData = require("../controllers/fetchuserdatacontroller");
 
-router.post("/userData",UserData);
+router.post("/userData",UserData.UsersData);
+router.get("/requser/:username",UserData.GetUsersData);
 
 module.exports = router;
