58926aa8054a9eedeaaf96babc2aa3dadb34285d
Shashika616 <shashikauda@gmail.com>
2024-01-30 11:18:56 +0530

    change of export in model

--- models/userModel.js
+++ models/userModel.js
@@ -32,4 +32,8 @@ const userSchema = new Schema({
    
 });
 
-module.exports = mongoose.model('User', userSchema);
+//module.exports = mongoose.model('User', userSchema);
+
+const User = mongoose.model('User', userSchema);
+
+    module.exports = User;
