73bbd7bfd1664c72ac87e566df1dba691eb36fab
Shashika616 <shashikauda@gmail.com>
2024-01-30 11:05:23 +0530

    spacing, names, error handling updated and modified

--- controllers/userstaffcontroller.js
+++ controllers/userstaffcontroller.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const Userstaff = require("../models/userstaffModel");
+const UserStaff = require("../models/userstaffModel");
 const bcrypt = require('bcryptjs');  // Hash Library for password
 const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
@@ -12,40 +12,43 @@ const NODEMAILER_PASSWORD = process.env.NODEMAILER_PASSWORD;
 const RECEIVER_EMAIL = process.env.RECEIVER_EMAIL;
 
 const userstaffRegister = async(req,res)=>{
-    const {empID,name,email,contactNo,division,password,confirmpwd}= req.body;
+    const {empID,username,email,contactNo,division,password,confirmpwd}= req.body;
     
     try {
-
+        
            // EmployeeID format check
         const empIDformat = /^\d{8}$/;
         if(!empIDformat.test(empID)){
             console.log("Invalid format for a Employee Number");
             return res.json({error: 'Invalid Employee Number'});
         }
-    
+
             // Fullname format check
-        if (!name || name.trim() === "") {
+        if (!username || username.trim() === "") {
             console.log("Enter Your Full Name here");
             return res.send({ error: 'Required' });
           }
-    
+
             // Email Validation
         if(!emailValidator.validate(email)){
             console.log("Not a valid Email");
             return res.send({error:'Invalid Email check again'});
     
         }
+
     
-        const usedEmail = await Userstaff.findOne({email});
+        const usedEmail = await UserStaff.findOne({email});
         if(usedEmail){
             return res.json({error:"This Email has already taken by another User"});
         }
     
+
             // Username used or not check
-        const usedUsername = await Userstaff.findOne({username});
+        const usedUsername = await UserStaff.findOne({username});
         if(usedUsername){
             return res.json({error:"This Usename is already being used try a different one"});
         }
+
     
             // Password format check
         const passwordformat = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/;
@@ -53,34 +56,41 @@ const userstaffRegister = async(req,res)=>{
             console.log(" At least 6 characters  including uppercase, lowercase, and numeric characters");
             return res.send({error:"Password must consists at least 6 characters with uppercase, lowercase, and numeric characters"});
         }
+
     
         if (password !== confirmpwd) {
             console.log("Passwords Don.t Match");   //Double check the password
             return res.json({ error: 'Passwords do not match' });
           }
-        
+
             // Contact number format check
         const contactNoFormat = /^\d{10}$/;
         if(!contactNoFormat.test(contactNo)){
             console.log("Invalid format for a mobile Number");
             return res.json({error: 'Enter Invalid Mobile Number'});
         }
+
         
             // encrypt password
-        const encryptedPassword = await bcrypt.hash(password,10);
+        const encryptedPassword = await bcrypt.hash(password,10); 
+
     
-        await Userstaff.create({
+        await UserStaff.create({
             empID,
-            name,
+            username,
             email,
             contactNo,
             division,
             password:encryptedPassword,
+
         });
+
+            
 
         res.send({status:"ok"}) 
         }catch (error) {
             res.send({status:"error"})
+            console.log(error);
         }
     };
 
