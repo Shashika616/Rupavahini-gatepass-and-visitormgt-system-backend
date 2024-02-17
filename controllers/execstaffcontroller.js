b84accf94b1caf8bea6f12b3630c8c6bf4393a25
Shashika616 <shashikauda@gmail.com>
2024-02-17 14:57:11 +0530

    controller for executive staff

---
+++ controllers/execstaffcontroller.js
@@ -0,0 +1,15 @@
+const execStaff = require('../models/executivestaffModel');
+
+const getexecstaff = async (req, res) => {
+    try {
+      const execstaff = await execStaff.find({});
+      res.status(200).json(execstaff);
+    } catch (error) {
+      console.error("Error retrieving data", error);
+      res.status(500).json({ error: "Could not retrieve executive staff data" });
+    }
+  };
+
+  module.exports = {
+    getexecstaff,
+  };
