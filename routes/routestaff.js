39b34c082e5684baef979968897ae6c6b3f035d5
Shashika616 <shashikauda@gmail.com>
2024-02-18 18:07:09 +0530

    route for editing staff data

--- routes/routestaff.js
+++ routes/routestaff.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const {userstaffRegister} = require("../controllers/userstaffcontroller");
+const {userstaffRegister, updateStaff } = require("../controllers/userstaffcontroller");
 
 router.post("/userstaffRegister",userstaffRegister);
+router.put('/userstaff/:empID', updateStaff);
 
 module.exports = router;
