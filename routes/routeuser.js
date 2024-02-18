1560f9e2bc1b55697c7ace15831279f753cf0e89
Shashika616 <shashikauda@gmail.com>
2024-02-18 18:04:22 +0530

    change of post request to a put request of the user update

--- routes/routeuser.js
+++ routes/routeuser.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const {userRegister, updateUser} = require("../controllers/usercontroller");
 
 router.post("/userRegister",userRegister);
-router.post('/update-user/:username',updateUser );
+router.put('/update-user/:username',updateUser );
 
 module.exports = router;
