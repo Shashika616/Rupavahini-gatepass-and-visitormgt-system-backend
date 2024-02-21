31641703e9042088bea308117bd09c69726a83e2
Shashika616 <shashikauda@gmail.com>
2024-02-21 11:37:03 +0530

    route file for user with the update password endpoint

--- routes/routeuser.js
+++ routes/routeuser.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const multer = require('multer');
 
-const {userRegister, updateUser} = require("../controllers/usercontroller");
+const {userRegister, updateUser, updatePassword } = require("../controllers/usercontroller");
 
 const upload = multer({ dest: 'uploads/' });
 
 router.post("/userRegister",userRegister);
 router.put('/update-user/:username',upload.single('image'),updateUser );
+router.put('/updateuserpwd/:username', updatePassword);
 
 module.exports = router;
