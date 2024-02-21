d731d97c7bceb3c5524439a481f8b3ae229fe75a
Shashika616 <shashikauda@gmail.com>
2024-02-21 14:22:29 +0530

    routes for the controller to handle new email verification

--- routes/routestaff.js
+++ routes/routestaff.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const multer = require('multer');
 
-const {userstaffRegister, updateStaff, updatePassword } = require("../controllers/userstaffcontroller");
+const {userstaffRegister, updateStaff, updatePassword, signup, verifyEmail } = require("../controllers/userstaffcontroller");
 
 const upload = multer({ dest: 'uploads/' });
 
 router.post("/userstaffRegister",userstaffRegister);
 router.put('/userstaff/:empID',upload.single('image'), updateStaff);
 router.put('/updatestaffpassword/:empID',updatePassword);
+router.post('/signupstaff', signup);
+router.post('/verify-email-staff', verifyEmail);
 
 module.exports = router;
--- routes/routeuser.js
+++ routes/routeuser.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const multer = require('multer');
 
-const {userRegister, updateUser, updatePassword } = require("../controllers/usercontroller");
+const {userRegister, updateUser, updatePassword, signup, verifyEmail } = require("../controllers/usercontroller");
 
 const upload = multer({ dest: 'uploads/' });
 
 router.post("/userRegister",userRegister);
 router.put('/update-user/:username',upload.single('image'),updateUser );
 router.put('/updateuserpwd/:username', updatePassword);
+router.post('/signupuser', signup);            // Signup with email verification
+router.post('/verify-email-user', verifyEmail);     // The endpoint for the verification page where we enter the verification code
 
 module.exports = router;
