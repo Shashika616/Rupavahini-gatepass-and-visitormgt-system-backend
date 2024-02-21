ec1967c0d475ff2b0503a97795523a47bc47d1ce
Shashika616 <shashikauda@gmail.com>
2024-02-21 14:20:03 +0530

    routes for get the requests by year, year+month, year+month+day

--- routes/routeStaffReq.js
+++ routes/routeStaffReq.js
@@ -8,4 +8,13 @@ router.get('/get/:empID', staffRequestController.getStaffRequestByID);
 router.get('/get/:empID/:currentDate', staffRequestController.getStaffRequestByIDAndDate);
 router.get('/get/:empID/today', staffRequestController.getStaffRequestByIDAndToday);
 
+// Route to get requests by year
+router.get('/requests/:year', staffRequestController.getRequestsByYear);
+
+// Route to get requests by year and month
+router.get('/requests/:year/:month', staffRequestController.getRequestsByYearAndMonth);
+
+// Route to get requests by year, month, and day
+router.get('/requests/:year/:month/:day', staffRequestController.getRequestsByYearMonthAndDay);
+
 module.exports = router;
