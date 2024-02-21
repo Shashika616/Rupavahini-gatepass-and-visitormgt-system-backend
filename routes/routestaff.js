080cde02eef1ca78c88ce3bb32c5c7393c2fc3c3
Shashika616 <shashikauda@gmail.com>
2024-02-21 11:38:29 +0530

    route file for staff with update password api

--- routes/routestaff.js
+++ routes/routestaff.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const multer = require('multer');
 
-const {userstaffRegister, updateStaff } = require("../controllers/userstaffcontroller");
+const {userstaffRegister, updateStaff, updatePassword } = require("../controllers/userstaffcontroller");
 
 const upload = multer({ dest: 'uploads/' });
 
 router.post("/userstaffRegister",userstaffRegister);
 router.put('/userstaff/:empID',upload.single('image'), updateStaff);
+router.put('/updatestaffpassword/:empID',updatePassword);
 
 module.exports = router;
