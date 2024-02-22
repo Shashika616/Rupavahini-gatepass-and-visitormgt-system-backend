833acc1333048aa08bd453e3df4b092845020412
dinukrana2000 <dinuk2000rana@gmail.com>
2024-02-22 19:21:06 +0530

    Revert "the endpoint for new update email verification"
    This reverts commit 5d6d63e9cfde8bf88a5848a3dd22a815238e7921.

--- routes/routeuser.js
+++ routes/routeuser.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require('multer');
 
-const {userRegister, updateUser, updatePassword, signup, verifyEmail, verifyEmailwhenupdate } = require("../controllers/usercontroller");
+const {userRegister, updateUser, updatePassword, signup, verifyEmail } = require("../controllers/usercontroller");
 
 const upload = multer({ dest: 'uploads/' });
 
@@ -11,6 +11,5 @@ router.put('/update-user/:username',upload.single('image'),updateUser );
 router.put('/updateuserpwd/:username', updatePassword);
 router.post('/signupuser', signup);            // Signup with email verification
 router.post('/verify-email-user', verifyEmail);     // The endpoint for the verification page where we enter the verification code
-router.post('/verify-email-user-update', verifyEmailwhenupdate);  // The endpoint for the update user email verification
 
 module.exports = router;
