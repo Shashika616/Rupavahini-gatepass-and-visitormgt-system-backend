e5fccd28eb9b57c99d126c60162c54e12e43a51c
dinukrana2000 <dinuk2000rana@gmail.com>
2024-03-02 00:25:42 +0530

    add controller and route for staff fogot password

--- controllers/staffforgotpasswordcontroller.js
+++ controllers/staffforgotpasswordcontroller.js
@@ -1,12 +1,109 @@
+require('dotenv').config();
+
 const Userstaff = require("../models/userstaffModel");
 const bcrypt = require('bcryptjs');  // Hash Library for password
 const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
 const emailValidator = require("email-validator");
+const UserStaff = require('../models/userstaffModel');
 
 const JWT_SECRET = process.env.JWT_SECRET;
 const NODEMAILER_USER = process.env.NODEMAILER_USER;
 const NODEMAILER_PASSWORD = process.env.NODEMAILER_PASSWORD;
-const RECEIVER_EMAIL = process.env.RECEIVER_EMAIL;
 
-// code to be implemented
+
+const staffForgetPassword = async (req, res) => {
+    const { email } = req.body;
+    try {
+        const oldUser = await Userstaff.findOne({ email });
+        if (!oldUser) {
+            return res.json({ status: "User Not Exists!!" });
+        }
+        const secret = JWT_SECRET + oldUser.password;
+        const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, {
+            expiresIn: "5m",
+        });
+        const link = `http://localhost:3000/staffchange/${oldUser._id}/${token}`;
+
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: NODEMAILER_USER, // Replace with your Gmail email
+                pass: NODEMAILER_PASSWORD // Replace with your Gmail password
+            }
+        });
+
+        var mailOptions = {
+            from: NODEMAILER_USER, // Replace with your Gmail email
+            to: oldUser.email,
+            subject: 'Password Reset',
+            text: link
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                res.send({ status: "Email has been sent" });
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Something went wrong" });
+    }
+};
+
+// Reset Password Before Submit Controller
+const staffResetPasswordBeforeSubmit = async (req, res) => {
+    const { id, token } = req.params;
+    try {
+        const oldUser = await UserStaff.findOne({ _id: id });
+        if (!oldUser) {
+            return res.json({ status: "User Not Exists!!" });
+        }
+        const secret = JWT_SECRET + oldUser.password;
+        const verify = jwt.verify(token, secret);
+        res.json({ email: verify.email, status: "Not Verified" });
+    } catch (error) {
+        console.error(error);
+        res.status(400).send("Link is Expired");
+    }
+};
+
+// Reset Password After Submit Controller
+const staffResetPasswordAfterSubmit = async (req, res) => {
+    const { id, token } = req.params;
+    const { password, confirmpassword } = req.body;
+    if (password !== confirmpassword) {
+        return res.status(400).json({ status: "Password not matched with confirm password" });
+    }
+    try {
+        const oldUser = await UserStaff.findOne({ _id: id });
+        if (!oldUser) {
+            return res.json({ status: "User Not Exists!!" });
+        }
+        const secret = JWT_SECRET + oldUser.password;
+        const verify = jwt.verify(token, secret);
+        const encryptedPassword = await bcrypt.hash(password, 10);
+        await UserStaff.updateOne(
+            {
+                _id: id,
+            },
+            {
+                $set: {
+                    password: encryptedPassword,
+                },
+            }
+        );
+        res.json({ email: verify.email, status: "Password Updated" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Something Went Wrong");
+    }
+};
+
+module.exports = {
+    staffForgetPassword,
+    staffResetPasswordBeforeSubmit,
+    staffResetPasswordAfterSubmit
+};
--- routes/routeforgotpassword.js
+++ routes/routeforgotpassword.js
@@ -6,6 +6,12 @@ const {
     ResetPasswordAfterSubmit
 } = require('../controllers/userforgotpasswordcontroller');
 
+const {
+    staffForgetPassword,
+    staffResetPasswordBeforeSubmit,
+    staffResetPasswordAfterSubmit
+}=require('../controllers/staffforgotpasswordcontroller');
+
 // Route for initiating password reset
 router.post('/forget-password', ForgetPassword);
 
@@ -14,5 +20,14 @@ router.get('/reset-password/:id/:token', ResetPasswordBeforeSubmit);
 
 // Route for handling submission of the new password
 router.post('/reset-password/:id/:token', ResetPasswordAfterSubmit);
+
+// Route for initiating password reset
+router.post('/staffforget-password', staffForgetPassword);
+
+// Route for rendering the page where users can input the new password
+router.get('/staffreset-password/:id/:token', staffResetPasswordBeforeSubmit);
+
+// Route for handling submission of the new password
+router.post('/staffreset-password/:id/:token', staffResetPasswordAfterSubmit);
 
 module.exports = router;
